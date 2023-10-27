41d044f18679a7d17cf97a5d5c654443c56427b2
Ericdid <ericdidom@gmail.com>
2023-10-27 17:35:00 +0200

    fix.route

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PageController::class, 'index'])->name('home');
 
 Route::get('/comics', [ComicController::class, 'index'])->name('comics');
-Route::resource('/comics', ComicController::class);
